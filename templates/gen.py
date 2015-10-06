0e3f2d1f0da4447c4ec10779dae95ce8f328a0df
pplinlin2 <pplinlin2@gmail.com>
2015-10-06 09:33:16 +0800

    fs

--- templates/gen.py
+++ templates/gen.py
@@ -4,14 +4,15 @@ import pypandoc
 from jinja2 import Template
 
 path = {
+	'path_line':     '../line', 
 	'path_wlbc':     '../wlbc', 
+	'path_porting':  '../porting_base', 
+	'path_fs':       '../fs', 
 	'path_driver':   '../linux_driver_basic', 
 	'path_advanced': '../linux_driver_advanced', 
 	'path_input':    '../input', 
 	'path_usb':      '../usb', 
 	'path_wk':       '../wk', 
-	'path_porting':  '../porting_base', 
-	'path_line':     '../line', 
 }
 
 with open('tables.txt', 'r') as f:
